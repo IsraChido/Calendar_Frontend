18327645ee91e296246c2b3129037eb670b36073
Jesús de Israel Jiménez Tostado <israeljimenezcontacto@gmail.com>
2024-03-23 18:05:47 -0700

    Pruebas añadidas

---
+++ tests/fixtures/calendarStates.js
@@ -0,0 +1,36 @@
+
+
+export const events = [
+    {
+        id: '1',
+        start: new Date('2022-10-21 13:00:00'),
+        end: new Date('2022-10-21 15:00:00'),
+        title: 'Cumpleaños de Israel',
+        notes: 'Alguna nota de Israel',
+    },
+    {
+        id: '2',
+        start: new Date('2022-11-09 13:00:00'),
+        end: new Date('2022-11-09 15:00:00'),
+        title: 'Cumpleaños de Jesús',
+        notes: 'Alguna nota de Jesús',
+    },
+];
+
+export const initialState = {
+    isLoadingEvents: true,
+    events: [],
+    activeEvent: null,
+}
+
+export const calendarWithEventsState = {
+    isLoadingEvents: false,
+    events: [...events],
+    activeEvent: null,
+}
+
+export const calendarWithActiveEventState = {
+    isLoadingEvents: false,
+    events: [...events],
+    activeEvent: {...events[0]},
+}
---
+++ tests/fixtures/testUser.js
@@ -0,0 +1,8 @@
+
+
+export const testUserCredentials = {
+    email: 'test@google.com',
+    password: '123456',
+    uid: '65fde9a95c7cf32bed361f41',
+    name: 'Test User',
+}
